e300b1811db65ee7fe5a950d07e4e1377a24136c
Sebastian OM <sebastianobandomorales@gmail.com>
2024-05-24 15:38:20 -0500

    adding sensors

--- FinalProject/generacion_0/generacion_1/generacion_2/SmartCity/components/sensors.cpp
+++ FinalProject/generacion_0/generacion_1/generacion_2/SmartCity/components/sensors.cpp
@@ -1,1 +1,59 @@
-// TODO
+#include <Arduino.h>
+#include <math.h> //Mathematics library for pow function (CO2 computation)
+
+double measureLight(int pin) {
+  double light_measurement = analogRead(pin);
+  Serial.println("Light pin: " + String(pin));
+  Serial.println("Light measurement: " + String(light_measurement));
+  return light_measurement
+}
+
+bool validateProximity(int pin) {
+
+  bool is_anything_there = false
+
+    if (digitalRead(pin) == HIGH) {
+    Serial.println("P1: ON");
+    delay(300);                         //Debouncing for buttons using delay of 300 ms
+    while (digitalRead(P1) == HIGH) {}  //Debouncing
+  }
+
+  return is_anything_there
+}
+
+bool validateButtons(int pin) {
+
+  bool is_button_on = false if (digitalRead(pin) == HIGH) {
+    Serial.println("P1: ON");
+    is_button_on = true delay(300);     //Debouncing for buttons using delay of 300 ms
+    while (digitalRead(P1) == HIGH) {}  //Debouncing
+  }
+
+  return is_button_on
+}
+
+
+double measureCO2(int pin) {
+
+  //->CO2
+  const float DC_GAIN = 8.5;  //define the DC gain of amplifier CO2 sensor
+  //const float ZERO_POINT_VOLTAGE = 0.4329; //define the output of the sensor in volts when the concentration of CO2 is 400PPM
+  const float ZERO_POINT_VOLTAGE = 0.265;                                                     //define the output of the sensor in volts when the concentration of CO2 is 400PPM
+  const float REACTION_VOLTAGE = 0.059;                                                       //define the “voltage drop” of the sensor when move the sensor from air into 1000ppm CO2
+  const float CO2Curve[3] = { 2.602, ZERO_POINT_VOLTAGE, (REACTION_VOLTAGE / (2.602 - 3)) };  //Line curve with 2 points
+
+  //Variable definitions
+  char comm = '\0';  //Command to test an actuator or sensor
+  float volts = 0;   //Variable to store current voltage from CO2 sensor
+  float co2 = 0;     //Variable to store CO2 value
+
+  volts = analogRead(pin) * 5.0 / 1023.0;  //Convert CO2 ADC to volts
+  if (volts / DC_GAIN >= ZERO_POINT_VOLTAGE) {
+    Serial.println("Error reading CO2");
+  } else {
+    co2 = pow(10, ((volts / DC_GAIN) - CO2Curve[1]) / CO2Curve[2] + CO2Curve[0]);
+    Serial.println("CO2: " + String(co2));
+  }
+
+  return co2
+}
--- FinalProject/generacion_0/generacion_1/generacion_2/SmartCity/components/testSensors.cpp
+++ FinalProject/generacion_0/generacion_1/generacion_2/SmartCity/components/testSensors.cpp
@@ -1,1 +1,38 @@
-// TODO
+void testChangeTrafficLightStateByLightColor(int pin_light_sensors[], int pin_buttons[], int co2, int pin_proximity_sensors[]) {
+
+  /*
+  LDR1 0 //LDR Light sensor from traffic light 1 connected in pin A0
+  LDR2 1  //LDR Light sensor from traffic light 2 connected in pin A1 
+  */
+  for (int pin_light_sensor : pin_light_sensors) {
+    double light_measure = measureLight(pin_light_sensor);
+    System.println("Checking light measure: " + String(light_measure);
+  }
+
+  /*
+  P1 37  //Traffic light 1 button connected in pin 37
+  P2 36  //Traffic light 2 button connected in pin 36    
+  */
+  for (int pin_button : pin_buttons) {
+    bool is_button_on = validateButtons(pin_button);
+    System.println("Checking buttons: " + String(is_button_on);
+  }
+  
+  //CO2 sensor connected in pin A3
+  double co2 = measureCO2(co2);
+  System.println("Checking CO2: " + String(co2));
+
+  /*
+  CNY1 35 //Infrared sensor 1 in traffic light 1 connected in pin 35
+  CNY2 34 //Infrared sensor 2 in traffic light 1 connected in pin 34
+  CNY3 33 //Infrared sensor 3 in traffic light 1 connected in pin 33
+  CNY4 32 //Infrared sensor 4 in traffic light 2 connected in pin 32
+  CNY5 31 //Infrared sensor 5 in traffic light 2 connected in pin 31
+  CNY6 30 //Infrared sensor 6 in traffic light 2 connected in pin 30
+ */
+  for (int pin_light_sensor : pin_light_sensors) {
+    bool is_anything_there = validateProximity(pin);
+    System.println("Checking sensor proximity: " + String(is_anything_there));
+  }
+
+}
